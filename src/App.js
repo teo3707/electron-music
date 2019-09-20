149897b832fcdd886becade46d7e6712d936f025
Newt <i3707@outlook.com>
2019-09-20 14:20:07 +0800

    标题栏ui、滚动条ui调整

--- src/App.js
+++ src/App.js
@@ -2,21 +2,24 @@ const electron = require('electron');
 
 class App {
 
-  constructor() {
-    this.win = null;
-  }
+    constructor() {
+        this.win = null;
+    }
 
-  createWindow() {
-      this.win = new electron.BrowserWindow({
-        width: 1000,
-        height: 670,
-        webPreferences: {
-          nodeIntegration: true,
-        }
-      });
-      // this.win.setProgressBar(0.5);
-      this.win.loadFile('index.html');
-  }
+    createWindow() {
+        this.win = new electron.BrowserWindow({
+            width: 1000,
+            height: 670,
+            minWidth: 1000,
+            minHeight: 670,
+            webPreferences: {
+                nodeIntegration: true,
+            },
+            titleBarStyle: 'hiddenInset',
+        });
+        // this.win.setProgressBar(0.5);
+        this.win.loadFile('index.html');
+    }
 }
 
 
